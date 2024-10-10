e24bbf4276d648e5cce5c94ad22afb9e2aa7c0e6
hi3816 <wlgp2534@naver.com>
2024-10-10 22:44:31 +0900

    UPD

---
+++ Assets/Script/Entities/Controllers/AnimationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public partial class PlayerInputController
+{
+    public class AnimationController : MonoBehaviour
+    {
+        protected Animator animator;
+        protected TownController controller;
+
+        protected virtual void Awake()
+        {
+            animator = GetComponentInChildren<Animator>();
+            controller = GetComponent<TownController>();
+        }
+    }
+}
